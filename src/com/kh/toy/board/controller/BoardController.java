6ee268b0b786a7c0c86465d84ca2953bf0bfa998
duddns5@naver.com <duddns5@naver.com>
2021-11-03 13:06:13 +0900

    [commit]ok

--- src/com/kh/toy/board/controller/BoardController.java
+++ src/com/kh/toy/board/controller/BoardController.java
@@ -37,6 +37,7 @@ public class BoardController extends HttpServlet {
 		System.out.println("fkfkfk");
 		System.out.println("qwer");
 		System.out.println("qqqq");
+		System.out.println("asdf");
 	}
 
 	/**
